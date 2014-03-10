d6e57c400f2a1fea0b29c8b3a1b6c272c67a5ed1
PATRIK F BAKKEN <bakken.patrik@gmail.com>
2014-03-10 15:40:07 +0100

    skjelett, ikke helt ferdig

---
+++ CalendarSystem/src/tdt4140/calendarsystem/Appointment.java
@@ -0,0 +1,5 @@
+package tdt4140.calendarsystem;
+
+public class Appointment {
+
+}
---
+++ CalendarSystem/src/tdt4140/calendarsystem/Manager.java
@@ -0,0 +1,29 @@
+package tdt4140.calendarsystem;
+
+public class Manager {
+
+	public Manager()
+	{
+		
+	}
+	
+	public void saveXML(String filename, String XMLString)
+	{
+		
+	}
+	
+	public String readXML(String filename)
+	{
+		return "";
+	}
+	
+	public String parseToXML()
+	{
+		return "";
+	}
+	
+	public void parseFromXML()
+	{
+		
+	}
+}
