3c2c35826bcc284580b1a0201e4d48ce272160a3
PATRIK F BAKKEN <bakken.patrik@gmail.com>
2014-03-13 12:24:37 +0100

    added constructor til User

--- CalendarSystem/src/tdt4140/calendarsystem/User.java
+++ CalendarSystem/src/tdt4140/calendarsystem/User.java
@@ -15,6 +15,13 @@ public class User {
 		
 	}
 	
+	public User(String name, String username, String password, String email) {
+		this.name = name;
+		this.username = username;
+		this.password = password;
+		this.email = email;
+	}
+	
 	/**
 	 * Checks equality of two User objects
 	 * @param aUser The user to compare to
