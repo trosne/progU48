b2a21d1286cdd32bcf6770de1479131762151267
PATRIK F BAKKEN <bakken.patrik@gmail.com>
2014-03-13 13:02:47 +0100

    Added equality methods

--- CalendarSystem/src/tdt4140/calendarsystem/Reservation.java
+++ CalendarSystem/src/tdt4140/calendarsystem/Reservation.java
@@ -8,11 +8,30 @@ public class Reservation {
 	Date start;
 	Date end;
 	
+	/**
+	 * Default constructor
+	 */
+	public Reservation() {
+		
+	}
+	
 	public Reservation(MeetingRoom room, Date start, Date end)
 	{
 		this.room = room;
 		this.start = start;
 		this.end = end;
+	}
+	
+	/**
+	 * Checks equality of two Reservation objects. They are equal if room, start and end are equal
+	 * @param res Reservation to compare to
+	 * @return true if equal, false if not
+	 */
+	public boolean isEqual(Reservation res) {
+		if (this.room.isEqual(res.getRoom()) && this.start.compareTo(res.getStart()) == 0 && this.end.compareTo(res.getEnd()) == 0)
+			return true;
+		
+		return false;
 	}
 
 	/**
