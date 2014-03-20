2b7c4f01b52e96ced16bd114d794460af5600e8e
PATRIK F BAKKEN <bakken.patrik@gmail.com>
2014-03-20 13:40:53 +0100

    lagt til ny test case of lagt inn noen små metoder for å hente info her og der

--- CalendarSystem/src/tdt4140/calendarsystem/XMLunitTest.java
+++ CalendarSystem/src/tdt4140/calendarsystem/XMLunitTest.java
@@ -19,8 +19,37 @@ public class XMLunitTest {
         calendarManager = new CalendarManager();
         userManager = new UserManager();
 
-        userManager.addUser("Katja Kaj", "kajkaj", "jeglikerboller", "katja@kaj.com");
-        userManager.addUser("Bente bent", "benben", "jeghaterboller", "bente@bent.co.uk");
+        /*userManager.addUser("Katja Kaj", "kajkaj", "jeglikerboller", "katja@kaj.com");
+        userManager.addUser("Bente Bent", "benben", "jeghaterboller", "bente@bent.co.uk");
+        userManager.addUser("Patrik Fridberg", "patrikfb", "1234", "bakken.patrik@gmail.com");
+        User one = new User("Vegard Brattsberg", "vegbrat", "2345", "vegard@brattsberg.no");
+        User two = new User("Lisa Steen", "lisasteen", "3456", "lisa@steen.com");
+        userManager.addUser(one);
+        userManager.addUser(two);
+        
+        Group _super = new Group("super");
+        userManager.addGroup(_super);
+        _super.addUserToGroup(one);
+        Group _sub = new Group("sub");
+        _super.addSubGroup(_sub);
+        _sub.addUserToGroup(two);
+        
+        userManager.parseToXML(); */
+        
+        userManager.parseFromXML();
+        
+        for (User user : UserManager.getInstance().getUsers()) {
+        	System.out.println("User: " + user.getName() + " " + user.getUsername() + " " + user.getPassword()
+        			+ " " + user.getEmail());
+        }
+        
+        for (Group group : UserManager.getInstance().getGroups()) {
+        	System.out.println("Group: " + group.getName() + " " + group.getUsers().size());
+        	
+        	for (Group subGroup : group.getSubGroups()) {
+        		System.out.println("Sub groups: " + subGroup.getName());
+        	}
+        }
 /*
         MeetingRoom room = new MeetingRoom();
         room.setCap(10);
@@ -44,6 +73,8 @@ public class XMLunitTest {
         roomManager.parseToXML();
         */
 
-        roomManager.parseFromXML("");
+        //roomManager.parseFromXML("");
+        
+        
     }
 }
